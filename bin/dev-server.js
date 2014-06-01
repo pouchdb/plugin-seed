39739b392029f19d985e8338618f7e37e414bb56
Nolan Lawson <nolan.lawson@gmail.com>
2014-05-31 22:34:41 -0700

    ensure tests start only when everything is ready

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -19,6 +19,10 @@ var w = watchify(indexfile);
 w.on('update', bundle);
 bundle();
 
+var filesWritten = false;
+var serverStarted = false;
+var readyCallback;
+
 function bundle() {
   var wb = w.bundle();
   wb.on('error', function (err) {
@@ -31,10 +35,14 @@ function bundle() {
     fs.rename(dotfile, outfile, function (err) {
       if (err) { return console.error(err); }
       console.log('Updated:', outfile);
+      filesWritten = true;
+      checkReady();
     });
   }
 }
+
 function startServers(callback) {
+  readyCallback = callback;
   // enable CORS globally, because it's easier this way
 
   var corsValues = {
@@ -63,15 +71,20 @@ function startServers(callback) {
     return http_server.createServer().listen(HTTP_PORT);
   }).then(function () {
     console.log('Tests: http://127.0.0.1:' + HTTP_PORT + '/test/index.html');
-    if (callback) {
-      callback();
-    }
+    serverStarted = true;
+    checkReady();
   }).catch(function (err) {
     if (err) {
       console.log(err);
       process.exit(1);
     }
   });
+}
+
+function checkReady() {
+  if (filesWritten && serverStarted && readyCallback) {
+    readyCallback();
+  }
 }
 
 if (require.main === module) {
